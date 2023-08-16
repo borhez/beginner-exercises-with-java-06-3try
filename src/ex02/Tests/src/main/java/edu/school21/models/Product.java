bef2408bf5f7859d437faa7df89b79f0576cc152
borhez <getlink32@gmail.com>
2023-08-16 15:51:39 +0300

    02: Product.java

---
+++ src/ex02/Tests/src/main/java/edu/school21/models/Product.java
@@ -0,0 +1,66 @@
+package edu.school21.models;
+
+import java.util.Objects;
+
+public class Product {
+    private Long id;
+    private String name;
+    private int price;
+
+    public Product(Long id, String name, int price) {
+        this.id = id;
+        this.name = name;
+        this.price = price;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getPrice() {
+        return price;
+    }
+
+    public void setPrice(int price) {
+        this.price = price;
+    }
+
+    @Override
+    public String toString() {
+        return "Product{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                ", price=" + price +
+                '}';
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj) return true;
+
+        if (obj == null || this.getClass() != obj.getClass()) {
+            return false;
+        }
+
+        Product product = (Product) obj;
+
+        return Objects.equals(id, product.id) && Objects.equals(name, product.name) && Objects.equals(price, product.price);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id, name, price);
+    }
+}
